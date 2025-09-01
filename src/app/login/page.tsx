2b9c9e1b50cf9eaf47da1c5267167ee7ebd7e144
Manoel <manoelja3301@gmail.com>
2025-09-01 20:00:35 -0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,4 @@
-import { Clock } from "lucide-react";
+aaaaaaaaaaa import { Clock } from "lucide-react";
 import { MagicLinkForm } from "../_components/magic-link-form";
 
 export default function MagicLinkPage() {
